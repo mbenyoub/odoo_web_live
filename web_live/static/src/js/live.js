19a523724ce305768a3daa4cba3b5d0c62a3ed63
jssuzanne <jssuzanne@localhost>
2014-07-10 12:47:25 +0200

    Update use bus_enhanced

--- web_live/static/src/js/live.js
+++ web_live/static/src/js/live.js
@@ -1,31 +1,15 @@
 (function() {
-    var web_live = openerp.web_live = {};
-
-    web_live.Notification = openerp.Widget.extend({
-        init: function(parent) {
-            var self = this;
-            this._super(parent);
-            this.bus = openerp.bus.bus;
-            this.channel = 'web_live'
-            this.bus.add_channel(this.channel)
-            this.bus.on("notification", this, this.on_notification);
-            this.bus.start_polling();
-        },
-        on_notification: function(notification) {
-            var channel = notification[0];
-            if (channel == this.channel) {
-                var message = notification[1];
+    openerp.web.WebClient.include({
+        declare_bus_channel: function() {
+            this._super();
+            var channel = 'web_live';
+            this.on(channel, this, function(message) {
                 var e = channel + '-' + message.model + '-';
                 if (message.kanban) {
                     this.trigger(e + 'kanban', message);
                 }
-            }
-        },
-    });
-    openerp.web.WebClient.include({
-        show_application: function() {
-            this._super();
-            this.web_live = new web_live.Notification(this);
+            });
+            this.add_bus_channel(channel);
         },
     });
 })();
